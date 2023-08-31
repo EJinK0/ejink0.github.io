692ceda486fb65ab1a79dbeaf17cc6a11eb97aa5
EJinK0 <wls5819@naver.com>
2023-08-31 22:32:13 +0900

    [DEL] 불필요 코드 정리

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Kakaomap from './pages/ScssExamplePage/KaKaomap';
+import SNSPage from './pages/SNSPage/SNSPage';
 
 function App() {
   return (
     <div>
-      <h1 className="My-header">MyStudy</h1>
-      <Link className='App-link' to="/mainpage">MainPage</Link> <br />
-      <Link className='App-link' to="/gamepage">GamePage</Link> <br />
-      <Link className='App-link' to="/snspage">SNSPage</Link> <br />
-      <Link className='App-link' to="/todolist">TodoList</Link> <br />
-      <Link className='App-link' to="/arrayExamplePage">ArrayExamplePage</Link> <br />
-      <Link className='App-link' to="/scssExamplePage">ScssExamplePage</Link> <br />
-      <Link className='App-link' to="/hookExamplePage">HookExamplePage</Link>
+      <SNSPage/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,32 +4,18 @@ import './index.scss';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
-import MainPage from './pages/MainPage/MainPage';
-import GamePage from './pages/GamePage/GamePage';
 import SNSPage from './pages/SNSPage/SNSPage';
 import Join from './pages/SNSPage/Join/Join';
-import TodoListPage from './pages/ToDoListPage/TodoListPage';
-import ArrayExamplePage from './pages/ArrayExamplePage/ArrayExamplePage';
 import MainFeed from './pages/SNSPage/MainFeed/MainFeed';
-import ScssExamplePage from './pages/ScssExamplePage/ScssExamplePage';
-import HookExamplePage from './pages/HookExamplePage/HookExamplePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />} />
-      <Route path='/mainpage' element={<MainPage />} />
-      <Route path='/gamepage' element={<GamePage />} />
-
-      <Route path='/snspage' element={<SNSPage />} />
-      <Route path='/join' element={<Join />} />
+      <Route path='/loginPage' element={<SNSPage />} />
+      <Route path='/joinPage' element={<Join />} />
       <Route path='/mainFeed' element={<MainFeed />} />
-
-      <Route path='/todolist' element={<TodoListPage />} />
-      <Route path='/arrayExamplePage' element={<ArrayExamplePage />} />
-      <Route path='/scssExamplePage' element={<ScssExamplePage />} />
-      <Route path='hookExamplePage' element={<HookExamplePage />} />
     </Routes>
   </BrowserRouter>
 );
